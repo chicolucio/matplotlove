63d6e9f439cf2f000b2fdd2617c6563928272584
Francisco Bustamante <chicolucio@gmail.com>
2022-02-14 12:00:25 -0300

    First 3D heart

--- matplotlove.py
+++ matplotlove.py
@@ -89,6 +89,84 @@ class Heart08(Heart07):
         super().show(blit=False)
 
 
+class Heart3d01:
+
+    def __init__(self):
+        self.fig, self.ax = self.plot_setup()
+
+    def plot_setup(self):
+        fig = plt.figure(figsize=(8, 8))
+        fig.tight_layout(pad=0)
+        ax = fig.add_subplot(111, projection='3d')
+        return fig, ax
+
+    def plot_implicit(self, fn, bbox=(-2.5, 2.5), resolution=100, slices=30,
+                      colormap=plt.cm.Reds):
+        """Create a plot of an implicit function fn
+        Adapted from https://stackoverflow.com/a/4687582/8706250
+
+        Parameters
+        ----------
+        fn : function
+            implicit funcion with fn == 0
+        bbox : tuple, optional
+            x, y, z limits, by default (-2.5, 2.5)
+        resolution : int, optional
+            number of points, by default 100
+        slices : int, optional
+            number of countour planes, by default 30
+        colormap : _type_, optional
+            Matplotlib colormap to be used, by default plt.cm.Reds
+        """
+
+        xmin, xmax, ymin, ymax, zmin, zmax = bbox * 3
+        A = np.linspace(xmin, xmax, resolution)
+        B = np.linspace(xmin, xmax, slices)
+        A1, A2 = np.meshgrid(A, A)
+        colors = [colormap(i) for i in np.linspace(0.0, 1.0, slices)]
+
+        for z, color in zip(B, colors):  # plot contours in the XY plane
+            X, Y = A1, A2
+            Z = fn(X, Y, z)
+            self.ax.contour(X, Y, Z+z, [z], zdir='z',
+                            colors=np.array(color).reshape(-1, 4))
+
+        for y, color in zip(B, colors):  # plot contours in the XZ plane
+            X, Z = A1, A2
+            Y = fn(X, y, Z)
+            self.ax.contour(X, Y+y, Z, [y], zdir='y',
+                            colors=np.array(color).reshape(-1, 4))
+
+        for x, color in zip(B, colors):  # plot contours in the YZ plane
+            Y, Z = A1, A2
+            X = fn(x, Y, Z)
+            self.ax.contour(X+x, Y, Z, [x], zdir='x',
+                            colors=np.array(color).reshape(-1, 4))
+
+        self.ax.set_zlim3d(zmin, zmax)
+        self.ax.set_xlim3d(xmin, xmax)
+        self.ax.set_ylim3d(ymin, ymax)
+        self.ax.grid(False)
+        self.ax.set_axis_off()
+
+    @staticmethod
+    def heart3D(x, y, z):
+        return (x**2 + (9/4) * y**2 + z**2 - 1)**3 - x**2 * z**3 - (9/80) * y**2 * z**3 < 0
+
+    def animate(self, i):
+        self.ax.view_init(elev=30, azim=3.6*i)
+        return self.fig,
+
+    def show(self):
+        self.plot_implicit(self.heart3D, bbox=(-1.3, 1.3),
+                           colormap=plt.cm.PuRd, slices=50)
+        ani = animation.FuncAnimation(self.fig, self.animate,
+                                      frames=100, interval=10, blit=False)
+
+        plt.tight_layout()
+        plt.show()
+
+
 if __name__ == "__main__":
     heart01 = Heart01()
     heart01.show(interval=1)
