04bf01b4e77db0c5b3dd4da92cd79d6185a1236a
Francisco Bustamante <chicolucio@gmail.com>
2022-02-14 10:17:16 -0300

    Class Heart01

--- matplotlove.py
+++ matplotlove.py
@@ -4,20 +4,42 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-x, y, t, a, b = sympy.symbols('x, y, t, a, b')
-
-heart_01 = sympy.Eq((x**2 + y**2 - a)**3 - x**2 * y**3, 0)
-
 
 def mid_value(interval): return interval.mid
 
 
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.grid(False)
-ax.set_axis_off()
+class Heart01:
 
+    def __init__(self):
+        self.x, self.y, self.a = sympy.symbols('x, y, a')
+        self.eq = sympy.Eq((self.x**2 + self.y**2 - self.a)
+                           ** 3 - self.x**2 * self.y**3, 0)
+        self.fig, self.ax = self.plot_setup()
 
-def animate(constant):
+    def plot_setup(self):
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.grid(False)
+        ax.set_axis_off()
+        return fig, ax
+
+    def animate(self, constant):
+
+        e = self.eq.subs({self.a: constant})
+        p = plot_implicit(e, show=False)
+
+        intervals = np.array(p[0].get_points()[0])
+        px = tuple(map(mid_value, intervals[:, 0]))
+        py = tuple(map(mid_value, intervals[:, 1]))
+        points = np.array((px, py))
+
+        heart = self.ax.scatter(*points)
+        return heart,
+
+    def show(self, frames=5, interval=10, blit=True):
+        ani = animation.FuncAnimation(self.fig, self.animate, frames=frames,
+                                      interval=interval, blit=blit)
+        plt.show()
+
 
     e = heart_01.subs({a: constant})
     p = plot_implicit(e, show=False)
@@ -27,8 +49,6 @@ def animate(constant):
     py = tuple(map(mid_value, intervals[:, 1]))
     points = np.array((px, py))
 
-    heart = ax.scatter(*points)
-    return heart,
 
 
 ani = animation.FuncAnimation(fig, animate, frames=5, interval=10,
