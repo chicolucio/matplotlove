4f5555b330861f7fd7445c825656297555966f7b
Francisco Bustamante <chicolucio@gmail.com>
2022-02-13 22:18:43 -0300

    First heart shape animation

--- matplotlove.py
+++ matplotlove.py
@@ -1,17 +1,24 @@
 import sympy
 from sympy.plotting import plot, plot_implicit
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 
 x, y, t, a, b = sympy.symbols('x, y, t, a, b')
 
 heart_01 = sympy.Eq((x**2 + y**2 - a)**3 - x**2 * y**3, 0)
-heart_01
 
-mid_value = lambda interval: interval.mid
-plots_heart_01 = []
 
-for constant in range(1, 6):
+def mid_value(interval): return interval.mid
+
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.grid(False)
+ax.set_axis_off()
+
+
+def animate(constant):
+
     e = heart_01.subs({a: constant})
     p = plot_implicit(e, show=False)
 
@@ -19,15 +26,11 @@ for constant in range(1, 6):
     px = tuple(map(mid_value, intervals[:, 0]))
     py = tuple(map(mid_value, intervals[:, 1]))
     points = np.array((px, py))
-    plots_heart_01.append(points)
 
-fig, ax = plt.subplots()
+    heart = ax.scatter(*points)
+    return heart,
 
-for i, p in enumerate(plots_heart_01, 1):
-    ax.scatter(*p, label=f'a = {i}')
-    # ax.set_ylim(0, 10)
-    ax.legend()
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
 
+ani = animation.FuncAnimation(fig, animate, frames=5, interval=10,
+                              blit=True)
 plt.show()
