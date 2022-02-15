012c55d904cda28c07a3e76e22435585d0f22c3b
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-02-14 21:48:58 -0300

    changed project structure
    - required for making it a installable package!
    - also, split heats.py from the main script for more readable code!

--- src/matplotlove/hearts.py
+++ src/matplotlove/hearts.py
@@ -185,36 +185,3 @@ class Heart3d02(Heart3d01):
 
         plt.tight_layout()
         plt.show()
-
-
-def cli():
-
-    while True:
-        try:
-            print('\n\nWelcome to matplotlove!! By chicolucio', end='\n\n')
-            print('Press 2 for 2D or 3 for 3D')
-            choice_2d_3d = input('Do you want a 2D or a 3D heart? ')
-        except KeyboardInterrupt:
-            print('\nNo more love :-(')
-            break
-        else:
-            if choice_2d_3d == '2':
-                options_2d = input('Choose a number between 1 and 4: ')
-                if int(options_2d) in range(1, 5):
-                    eval(f'Heart0{options_2d}().show()')
-                    break
-                else:
-                    print('Invalid number', end='\n\n')
-            elif choice_2d_3d == '3':
-                options_3d = input('Choose a number between 1 and 2: ')
-                if int(options_3d) in range(1, 3):
-                    eval(f'Heart3d0{options_3d}().show()')
-                    break
-                else:
-                    print('Invalid number', end='\n\n')
-            else:
-                print('Invalid answer', end='\n\n')
-
-
-if __name__ == "__main__":
-    cli()
